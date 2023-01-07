04c7dd58ed26f4819d59f2c19ceef92fef20174a
saisagrace <53611092+saisagrace@users.noreply.github.com>
2023-01-07 11:57:00 +0300

    second commit

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const petcareRoute = require('./routes/petcare');
 const MongoStore = require('connect-mongo');
 
 const app = express();
-if (process.env.NODE_ENV !== "production") {
-    app.use(express.static('client/build'));
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-}
+// if (process.env.NODE_ENV !== "production") {
+//     app.use(express.static('client/build'));
+//     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+// }
 const cors = require("cors");
 app.use(cors());
 
